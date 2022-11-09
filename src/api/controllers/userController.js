1a884488c864bdcb208119dedc50910bad2c3472
Kilian Pichard <kil.pichard@icloud.com>
2022-11-09 14:34:46 +0100

    Crypt password on register with bcrypt

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -3,46 +3,74 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 exports.userRegister = (req, res) => {
-    let newUser = new User(req.body);
-
-    newUser.save((error, user) => {
+    bcrypt.hash(req.body.password, 10, (error, hash) => {
         if (error) {
-            res.status(401);
-            console.log(error);
-            res.json({ message: "Reqûete invalide." });
+            return res.status(500).json({
+                error: error
+            });
+        } else {
+            req.body.password = hash;
+            const user = new User(req.body);
+
+            user.save((error, user) => {
+                if (error) {
+                    return res.status(500).json({
+                        error: error
+                    });
+                } else {
+                    return res.status(201).json({
+                        message: "User created successfully"
+                    });
+                }
+            });
         }
-        else {
-            res.status(201);
-            res.json({ message: `Utilisateur crée : ${user.email}` });
-        }
-    })
+    });
+};
 
 
 }
 
 exports.loginRegister = (req, res) => {
-    // Find user
-    User.findOne({ email: req.body.email }, (error, user) => {
-        // If user not found
-        if (error) {
-            res.status(500);
-            console.log(error);
-            res.json({ message: "Utilisateur non trouvé" });
-        }
-        else {
-            // User found
-            if (user.email == req.body.email && user.password == req.body.password) {
-                // Password correct
-                let userData = {
-                    id: user._id,
-                    email: user.email,
-                    role: "admin"
-                }
-                jwt.sign(userData, process.env.JWT_KEY, { expiresIn: "30 days" }, (error, token) => {
-                    if(error) {
-                        res.status(500);
-                        console.log(error);
-                        res.json({message: "Impossible de générer le token"});
+	// Find user
+	User.findOne({ email: req.body.email }, (error, user) => {
+		// If user not found
+		if (error) {
+			res.status(500);
+			console.log(error);
+			res.json({ message: "Utilisateur non trouvé" });
+		} else {
+			// User found
+			if (user.email === req.body.email && user.password === req.body.password) {
+				// Password correct
+				let userData = {
+					id: user._id,
+					email: user.email,
+					role: "admin",
+				};
+				jwt.sign(
+					userData,
+					process.env.JWT_KEY,
+					{ expiresIn: "30 days" },
+					(error, token) => {
+						if (error) {
+							res.status(500);
+							console.log(error);
+							res.json({ message: "Impossible de générer le token" });
+						} else {
+							res.status(200);
+							res.json({ token });
+						}
+					},
+				);
+			} else {
+				// Password don't match
+				res.status(401);
+				console.log(error);
+				res.json({ message: "Email ou Mot de passe incorrect" });
+			}
+		}
+	});
+};
 
                     }
                     else {
