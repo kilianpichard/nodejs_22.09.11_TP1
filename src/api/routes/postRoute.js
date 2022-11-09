d3dd436956d170712a373adcb7a425bdd032e01d
atacanjacques <atacan.jacques@gmail.com>
2022-11-09 14:23:38 +0100

    Add auth

--- src/api/routes/postRoute.js
+++ src/api/routes/postRoute.js
@@ -1,11 +1,12 @@
 module.exports = (server) => {
     const postController = require("../controllers/postController");
-
+    const jwtMiddleware = require("../middlewares/jwtMiddleware");
 server.route("/posts")
 .get(postController.listAllPosts)
-.post(postController.createAPost);
+.post(jwtMiddleware.verifyToken, postController.createAPost);
 
 server.route("/posts/:post_id") // req.params.post_id
+.all(jwtMiddleware.verifyToken)
 .get(postController.getAPost)
 .put(postController.updateAPost)
 .delete(postController.deleteApost);
