3038bd4ef366db64802d7e20637198f9830dbafa
Kilian Pichard <kil.pichard@icloud.com>
2022-11-09 15:21:48 +0100

    add isAdmin middleware

---
+++ src/api/middlewares/adminMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const jwtKey = process.env.JWT_KEY;
+
+exports.isAdmin = (req, res, next) => {
+	let token = req.headers["authorization"];
+	if (token !== undefined) {
+		jwt.verify(token, jwtKey, (error, payload) => {
+			if (error) {
+				console.log(error);
+				res.status(403);
+				res.json({ message: "Accès interdit : token invalide" });
+			} else {
+				if (payload.role === "admin") {
+					next();
+				} else {
+					res.status(403);
+					res.json({ message: "Accès interdit : vous n'êtes pas admin" });
+				}
+			}
+		});
+	} else {
+		res.status(403);
+		res.json({ message: "Accès interdit : token manquant" });
+	}
+};
